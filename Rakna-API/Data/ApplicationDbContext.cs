6993f06d6eb5a756cb9e79024e72196aa4c96852
Administrator <Administrator@EQPOCI-GAMEEL-R>
2023-02-28 12:15:06 +0200

    Add project files.

---
+++ Rakna-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rakna_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("Users", "security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "security");
+        }
+    }
+}
---
+++ Rakna-API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rakna_API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
